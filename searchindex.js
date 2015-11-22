73ffa8dc78ecf0786c560f344041b555a34b7bd5
Mariano Guerra <mariano@marianoguerra.org>
2015-11-23 00:45:42 +0100

    include build

---
+++ searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({envversion:42,terms:{content:[],index:[],modul:[],search:[],page:[]},objtypes:{},objnames:{},filenames:["introduction","index","reference"],titles:["Introduction","Interfix Programming Language","Reference"],objects:{},titleterms:{welcom:[],interfix:1,indic:[],program:1,tabl:[],document:[],refer:2,languag:1,introduct:0}})
